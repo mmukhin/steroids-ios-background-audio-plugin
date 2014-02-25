ae107ed3260df2e89d3cc51b8d74695ec5230ac6
Pirkka Esko <pirkka@gmail.com>
2014-02-25 14:49:32 +0200

    Update Echo.m
    and our method implementation

--- src/ios/Echo.m
+++ src/ios/Echo.m
@@ -20,3 +20,31 @@
 }
 
 @end
+
+@implementation AVSession
+
+- (void)setCategory:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* argumentOne = [command.arguments objectAtIndex:0];
+
+//    if (echo != nil && [echo length] > 0) {
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+//    } else {
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//    }
+
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    BOOL ok;
+    NSError *setCategoryError = nil;
+    ok = [audioSession setCategory:AVAudioSessionCategoryPlayback
+                           error:&setCategoryError];
+
+    // add error handling ... someday ^.^'' 
+
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Done setting AVAudioSessionCategoryPlayback."];
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
