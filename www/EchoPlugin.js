ff57ddb43c168ca10a4edad709d19f92a69c08d5
Pirkka Esko <pirkka@gmail.com>
2014-02-26 09:59:01 +0200

    Update EchoPlugin.js

--- www/EchoPlugin.js
+++ www/EchoPlugin.js
@@ -1,9 +1,3 @@
-window.echo = function(str, callback) {
-    cordova.exec(callback, function(err) {
-        callback('Nothing to echo.');
-    }, "Echo", "echo", [str]);
-};
-
 window.setAVSessionCategory = function(category, callback) {
     cordova.exec(callback, function(err) {
         callback('Something went wrong.');
