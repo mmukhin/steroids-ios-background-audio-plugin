313a36d5b74642667ce325eed4ea3b5e2f7ac263
Michael Mukhin <mukhin.m@gmail.com>
2015-03-27 18:01:07 -0700

    Update Echo.m

--- src/ios/Echo.m
+++ src/ios/Echo.m
@@ -41,26 +41,62 @@
 
 - (void)setNowPlayingInfo:(CDVInvokedUrlCommand*)command {
 
-    CDVPluginResult* pluginResult = nil;
-
-    NSString* artist = [command.arguments objectAtIndex:0];
+    	CDVPluginResult* pluginResult = nil;
+	
+	NSString* artist = [command.arguments objectAtIndex:0];
     NSString* title = [command.arguments objectAtIndex:1];
     NSString* album = [command.arguments objectAtIndex:2];
+    NSString *cover = [command.arguments objectAtIndex:3];
     
-  
-    MPNowPlayingInfoCenter *center = [MPNowPlayingInfoCenter defaultCenter];
-    NSDictionary *songInfo = [NSDictionary dictionaryWithObjectsAndKeys:
-                             artist, MPMediaItemPropertyArtist,
-                             title, MPMediaItemPropertyTitle,
-                             album, MPMediaItemPropertyAlbumTitle,
-                             artwork, MPMediaItemPropertyArtwork];
-    center.nowPlayingInfo = songInfo;
+	// async cover loading
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        UIImage *image = nil;
+        // check whether cover path is present
+        if (![cover isEqual: @""]) {
+            // cover is remote file
+            if ([cover hasPrefix: @"http://"] || [cover hasPrefix: @"https://"]) {
+                NSURL *imageURL = [NSURL URLWithString:cover];
+                NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
+                image = [UIImage imageWithData:imageData];
+            }
+            // cover is local file
+            else {
+                NSString *basePath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+                NSString *fullPath = [NSString stringWithFormat:@"%@%@", basePath, cover];
+                BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:fullPath];
+                if (fileExists) {
+                    image = [UIImage imageNamed:fullPath];
+                }
+            }
+        }
+        else {
+            // default named "no-image"
+            image = [UIImage imageNamed:@"no-image"];
+        }
+        // check whether image is loaded
+        CGImageRef cgref = [image CGImage];
+        CIImage *cim = [image CIImage];
+        if (cim != nil || cgref != NULL) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (NSClassFromString(@"MPNowPlayingInfoCenter")) {
+                    MPMediaItemArtwork *artwork = [[MPMediaItemArtwork alloc] initWithImage: image];
+                    MPNowPlayingInfoCenter *center = [MPNowPlayingInfoCenter defaultCenter];
+                    center.nowPlayingInfo = [NSDictionary dictionaryWithObjectsAndKeys:
+                        artist, MPMediaItemPropertyArtist,
+                        title, MPMediaItemPropertyTitle,
+                        album, MPMediaItemPropertyAlbumTitle,
+                        artwork, MPMediaItemPropertyArtwork
+                    ];
 
-    NSString *resultText = [NSString stringWithFormat:@"Set now playing info to %@", center.nowPlayingInfo];
-    
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:resultText];
+                    NSString *resultText = [NSString stringWithFormat:@"Set now playing info to %@", center.nowPlayingInfo];
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:resultText];
+
+                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                }
+            });
+        }
+    });
 }
 
 @end
